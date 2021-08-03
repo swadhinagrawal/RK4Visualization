526dfaa5a42eb03745a3f2942ed6f3f044e8fc18
Swadhin Agrawal <swadhin12a@gmail.com>
2021-08-03 21:19:34 +0530

    Add files via upload

---
+++ rk4.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.core.function_base import linspace
+
+
+def dfdx(x,t):
+    return x - np.sin(t)
+
+def inte(x,t):
+    return x*t + np.cos(t) - 1
+
+t = 0
+dt = 0.3
+t1 = 0
+dt1 = 0.01
+f0 = 0
+fx = [f0]
+tm = [t]
+tm1 = [t1]
+f = f0
+intfx = [inte(f0,t1)]
+count = 0
+fig = plt.figure()
+
+x = linspace(-1,1,100)
+plt.ion()
+while t<100:
+    while t1<t:
+        t1 += dt1
+        tm1.append(t1)
+        intfx.append(inte(f0,t1))
+    k1 = dfdx(f0,t)
+    c1 = plt.plot(x+t,f+k1*x,c='blue')
+    s1 = plt.scatter(t,f,c='blue')
+
+    k2 = dfdx(f0+ dt*k1/2,t+dt/2)
+    c2 = plt.plot(x+t+dt/2,f+dt*k1/2+k2*x,c='green')
+    s2 = plt.scatter(t+dt/2,f+dt*k1/2,c='green')
+
+    k3 = dfdx(f0+ dt*k2/2,t+dt/2)
+    c3 = plt.plot(x+t+dt,f+ dt*k2/2+k3*x,c='black')
+    s3 = plt.scatter(t+dt,f+dt*k1/2+dt*k2/2,c='black')
+
+    k4 = dfdx(f0+ dt*k3,t+dt)
+    c4 = plt.plot(x+t+3*dt/2,f+ dt*k3+k4*x,c='brown')
+    s4 = plt.scatter(t+3*dt/2,f+dt*k1/2+dt*k2/2+dt*k3/2,c='brown')
+
+    f += dt*(k1+2*k2+2*k3+k4)/6
+    t += dt
+    
+    fx.append(f)
+    tm.append(t)
+    plt.plot(tm,fx,color = 'red')
+    plt.plot(tm1,intfx,c='blue')
+    plt.pause(0.1)
+    c1.pop(0).remove()
+    s1.remove()
+    c2.pop(0).remove()
+    s2.remove()
+    c3.pop(0).remove()
+    s3.remove()
+    c4.pop(0).remove()
+    s4.remove()
+    plt.show()
+
+
+
