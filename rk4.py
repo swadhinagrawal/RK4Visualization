95a0ab2bccad180c1f6434a94f559b6a42caf431
Swadhin Agrawal <swadhin12a@gmail.com>
2021-08-14 12:42:00 +0530

    Update rk4.py

--- rk4.py
+++ rk4.py
@@ -4,55 +4,65 @@ from numpy.core.function_base import linspace
 
 
 def dfdx(x,t):
-    return x - np.sin(t)
+    return 4*x*t
 
-def inte(x,t):
-    return x*t + np.cos(t) - 1
+def inte(t):
+    return np.exp(2*(t**2))
+
 
 t = 0
 dt = 0.3
 t1 = 0
 dt1 = 0.01
-f0 = 0
+f0 = 1
 fx = [f0]
 tm = [t]
 tm1 = [t1]
 f = f0
-intfx = [inte(f0,t1)]
+intfx = [f0]
 count = 0
 fig = plt.figure()
 
-x = linspace(-1,1,100)
+
+
 plt.ion()
-while t<100:
+while t<10:
+    x = linspace(0,1,100)
+    f1 = f
     while t1<t:
         t1 += dt1
         tm1.append(t1)
-        intfx.append(inte(f0,t1))
-    k1 = dfdx(f0,t)
+        f1 = inte(t1)
+        intfx.append(f1)
+    plt.plot(tm1,intfx,c='purple')
+    k1 = dfdx(f,t)
     c1 = plt.plot(x+t,f+k1*x,c='blue')
     s1 = plt.scatter(t,f,c='blue')
 
-    k2 = dfdx(f0+ dt*k1/2,t+dt/2)
+    # plt.pause(0.1)
+    k2 = dfdx(f+dt*k1/2,t+dt/2)
     c2 = plt.plot(x+t+dt/2,f+dt*k1/2+k2*x,c='green')
     s2 = plt.scatter(t+dt/2,f+dt*k1/2,c='green')
 
-    k3 = dfdx(f0+ dt*k2/2,t+dt/2)
-    c3 = plt.plot(x+t+dt,f+ dt*k2/2+k3*x,c='black')
+    # plt.pause(0.1)
+    k3 = dfdx(f+dt*k2/2,t+dt/2)
+    c3 = plt.plot(x+t+dt,f+dt*k1/2+ dt*k2/2+k3*x,c='black')
     s3 = plt.scatter(t+dt,f+dt*k1/2+dt*k2/2,c='black')
 
-    k4 = dfdx(f0+ dt*k3,t+dt)
-    c4 = plt.plot(x+t+3*dt/2,f+ dt*k3+k4*x,c='brown')
+    # plt.pause(0.1)
+    k4 = dfdx(f+dt*k3,t+dt)
+    c4 = plt.plot(x+t+3*dt/2,f+dt*k1/2+ dt*k2/2+ dt*k3/2+k4*x,c='brown')
     s4 = plt.scatter(t+3*dt/2,f+dt*k1/2+dt*k2/2+dt*k3/2,c='brown')
 
+    # plt.pause(0.1)
     f += dt*(k1+2*k2+2*k3+k4)/6
     t += dt
     
     fx.append(f)
     tm.append(t)
     plt.plot(tm,fx,color = 'red')
-    plt.plot(tm1,intfx,c='blue')
-    plt.pause(0.1)
+    
+    plt.pause(1)
     c1.pop(0).remove()
     s1.remove()
     c2.pop(0).remove()
@@ -62,6 +72,3 @@ while t<100:
     c4.pop(0).remove()
     s4.remove()
     plt.show()
-
-
-
